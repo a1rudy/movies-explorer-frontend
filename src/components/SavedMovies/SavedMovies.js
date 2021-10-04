0c4b18fcc415844e77778373b58cdbe5aab4a165
Alexey Rudoy <ao.rudoy@yandex.ru>
2021-10-04 22:15:22 +0300

    fix: add SearchForm in SavedMovies & readme

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Preloader from '../Preloader/Preloader';
+import SearchForm from '../SearchForm/SearchForm';
 import MoviesCard from '../MoviesCard/MoviesCard';
 import image1 from '../../images/movies-card/movies-card-1.png';
 import image2 from '../../images/movies-card/movies-card-2.png';
@@ -8,6 +9,7 @@ import image3 from '../../images/movies-card/movies-card-3.png';
 function SavedMovies() {
   return (
     <section className="movies-card-list">
+      <SearchForm />
       {/* <Preloader /> */}
       <div className="movies-card-list__container">
         <MoviesCard isSaved={true} image={image1} />
